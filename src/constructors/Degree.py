90cb5b712bd69631eceb91c82a3e1697557a9a78
Rayla Kurosaki 『黒崎 レイラ』 <60267154+RaylaKurosaki1503@users.noreply.github.com>
2023-05-26 07:23:35 -0400

    Create Degree.py

---
+++ src/constructors/Degree.py
@@ -0,0 +1,22 @@
+"""
+Author: Rayla Kurosaki
+
+GitHub Username: RaylaKurosaki1503
+
+File: Degree.py
+"""
+
+
+class Degree:
+    def __init__(self, plan_code: str, name: str):
+        self.plan_code: str = plan_code
+        self.name: str = name
+        pass
+
+    def get_plan_code(self) -> str:
+        return self.plan_code
+
+    def get_name(self) -> str:
+        return self.name
+
+    pass
