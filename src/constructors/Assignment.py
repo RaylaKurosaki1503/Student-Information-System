95fed6cf1c95726b2add9c80aed24202fd1cef1b
Rayla Kurosaki 『黒崎 レイラ』 <60267154+RaylaKurosaki1503@users.noreply.github.com>
2023-05-26 07:23:34 -0400

    Create Assignment.py

---
+++ src/constructors/Assignment.py
@@ -0,0 +1,30 @@
+"""
+Author: Rayla Kurosaki
+
+GitHub Username: RaylaKurosaki1503
+
+File: Assignment.py
+"""
+
+
+class Assignment:
+    def __init__(self, grade: str, notes: str):
+        self.grade: str = grade
+        self.notes: str = notes
+        pass
+
+    def get_grade(self) -> str:
+        return self.grade
+
+    def set_grade(self, grade: str):
+        self.grade = grade
+        pass
+
+    def compute_grade(self) -> float:
+        num, denom = self.grade.split("/")
+        return float(num) / float(denom)
+
+    def get_notes(self) -> str:
+        return self.notes
+
+    pass
