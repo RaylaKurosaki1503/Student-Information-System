1723403f9950d738dba505ccec57732e2e7cfcca
Rayla Kurosaki 『黒崎 レイラ』 <60267154+RaylaKurosaki1503@users.noreply.github.com>
2023-05-26 09:00:44 -0400

    Update phase2.py

--- src/phases/phase2.py
+++ src/phases/phase2.py
@@ -52,11 +52,13 @@ def overwrite_final_exam_grade(student: Student, path_to_excel_file: str):
             pass
         course: Course = student.get_course(f"{term}.{course_code}.{section}")
         asgmt_1: AsgmtType = course.get_assignment_type("Final Exam")
-        final_exam: Asgmt = asgmt_1.get_assignments()[0]
-        exams: AsgmtType = course.get_assignment_type("Exams")
-        min_exam_grade: Asgmt = exams.get_min_grade()[1]
-        if final_exam.compute_grade() > min_exam_grade.compute_grade():
-            min_exam_grade.set_grade(final_exam.get_grade())
+        if len(asgmt_1.get_assignments()) > 0:
+            final_exam: Asgmt = asgmt_1.get_assignments()[0]
+            exams: AsgmtType = course.get_assignment_type("Exams")
+            min_exam_grade: Asgmt = exams.get_min_grade()[1]
+            if final_exam.compute_grade() > min_exam_grade.compute_grade():
+                min_exam_grade.set_grade(final_exam.get_grade())
+                pass
             pass
         pass
     pass
